9f2993102532c9983630e26a4398d614c567a12f
James Glover <james.glover@sanger.ac.uk>
2017-10-03 15:50:21 +0100

    ss502 - Allow filter by lot number

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -1,10 +1,21 @@
 class StandardsController < ApplicationController
   def index
-    @standards = Standard.include_for_list.latest_first.page(params[:page])
+    @filtered = filtered?
+    @standards = Standard.include_for_list.where(search_params).latest_first.page(params[:page])
   end
 
   def show
     @standard = Standard.include_for_list.with_barcode(params[:barcode]).first!
     @subtitle = @standard.barcode
+  end
+
+  private
+
+  def search_params
+    params.permit('lot_number').delete_if {|k,v| v.blank? }
+  end
+
+  def filtered?
+    search_params.present?
   end
 end
--- test/controllers/standards_controller_test.rb
+++ test/controllers/standards_controller_test.rb
@@ -9,6 +9,16 @@ class StandardsControllerTest < ActionController::TestCase
     assert assigns(:standards).include?(standard)
   end
 
+  test 'should filter by lot_number' do
+    standard = create :standard, lot_number: '123'
+    other_standard = create :standard, lot_number: '456'
+    get :index, lot_number: '123'
+    assert_response :success
+    assert_not_nil assigns(:standards)
+    assert assigns(:standards).include?(standard)
+    refute assigns(:standards).include?(other_standard)
+  end
+
   test 'should get show' do
     standard = create :standard
     get :show, barcode: standard.barcode
